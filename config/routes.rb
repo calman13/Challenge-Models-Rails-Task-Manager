c0ecba3e42ce167fd76078c94f57882d3fd815d6
osciel morales <osci.morales.m@gmail.com>
2021-07-21 12:34:03 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index' , as: :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  #Create
+  #get 'tasks/new', to: 'tasks#new', as: :new
+  #post 'tasks', to: 'tasks#create'
+
+  #Read
+  #get 'tasks', to: 'tasks#index' , as: :tasks
+  #get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #update
+  #get 'tasks/:id/edit', to: 'tasks#edit'
+  #patch 'tasks/:id', to: 'tasks#update'
+
+  #delete
+  #delete 'restaurants/:id', to: 'restaurants#destroy'
 end
